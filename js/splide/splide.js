2a4f1e2c8de8c9e925ca68ed21162280f53396c4
djcadaniel <djcadaniel@gmail.com>
2021-02-17 23:27:48 -0500

    entendiendo splide

--- js/splide/splide.js
+++ js/splide/splide.js
@@ -1,9 +1,33 @@
-new Splide(".splide").mount();
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '.splide',{
+        type: 'loop',
+        perPage: 3,
+        perMove:1,
+    //     height     : '30rem',
+	// cover      : true,
+        // focus: 'center',
+        // height   : '29rem',
+	    // autoWidth: true,
+        rewind : true,
+        autoplay:true,
+        interval:2000,
+        // focus    : 'center',
+        gap: 15,
+        easing: 'ease',
+        arrows: false,
+        pauseOnHover: true,
+        keyboard:true,
+        // direction  : 'ttb',
+        // heightRatio: 0.6,
+        cover: false,
+        breakpoints: {
+            640: {
+                perPage: 2,
+            },
+            480:{
+                perPage:1
+            }
+        }
+    } ).mount();
+} );
 
-document.addEventListener("DOMContentLoaded", function () {
-  new Splide(".splide"),
-    {
-      perPage: 3,
-      perMove: 2,
-    }.mount();
-});
